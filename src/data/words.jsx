2ec88b83ced3f0eb2619d302286e6df5b0d50e4d
Alan Paixao <alanpaixao2@gmail.com>
2023-05-18 16:28:17 -0300

    project secret word

---
+++ src/data/words.jsx
@@ -0,0 +1,8 @@
+export const wordsList = {
+  carro: ['Motor', 'Porta', 'Volante', 'Pneu', 'Antena'],
+  fruta: ['Banana', 'Goiaba', 'Tangerina', 'Abacate', 'Laranja'],
+  corpo: ['Cotovelo', 'Perna', 'Nariz', 'Panturrilha', 'Olhos'],
+  computador: ['Mouse', 'Teclado', 'Monitor', 'Gabinete'],
+  programação: ['Linguagem', 'Framework', 'JavaScript', 'React'],
+  alimento: ['Arroz', 'Carne', 'Leite', 'Ovo'],
+};
